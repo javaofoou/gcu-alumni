5a099231d0bc9026c575743f0f049fd6d8dd7b24
javaofoou <almaslahatilislamiyat@gmail.com>
2025-12-08 05:38:55 -0800

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,12 +13,11 @@ use Illuminate\Support\Facades\Auth;
 class AuthController extends Controller{
     public function showCreateAccount()
     {
-          dd(session()->all());
-    //     if (!session()->has('alumni_data')) {
-    //         return redirect('/alumni/register')->with('error', 'Please fill the alumni form first.');
-    //     }
-    //     return view('auth.register');
-    // }
+        if (!session()->has('alumni_data')) {
+            return redirect('/alumni/register')->with('error', 'Please fill the alumni form first.');
+        }
+        return view('auth.register');
+    }
 
     public function createAccount(Request $req)
     {
