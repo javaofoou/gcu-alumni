e7bef06f3a1f29a3bd41081b4321851adc5adea4
javaofoou <almaslahatilislamiyat@gmail.com>
2026-01-02 01:54:09 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,16 @@ Route::get('/award', [gcuPages::class, 'award'])->name('award');
 
 
 
+// routes/web.php
+Route::get('/clear-caches', function() {
+    Artisan::call('config:clear');
+    Artisan::call('cache:clear');
+    Artisan::call('route:clear');
+    Artisan::call('view:clear');
+    return 'Caches cleared!';
+});
+
+
 Route::get('/alumni/register', [AlumniRegisterController::class, 'showAlumniForm']);
 Route::post('/alumni/register', [AlumniRegisterController::class, 'storeAlumniSession'])->name('alumni.register.store');
 
